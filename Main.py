5fac4c071689cf468a8efb7cf9277c3b8e47571a
Xander Whitby <xwhitby20@gmail.com>
2024-06-14 11:24:23 -0600

    created game class

---
+++ Main.py
@@ -0,0 +1,24 @@
+import pygame
+import random
+
+pygame.init()
+
+
+class Game:
+    def __init__(self, screen, clock, bird, pipes, score):
+        self.screen = screen
+        self.clock = clock
+        self.bird = bird
+        self.pipes = score
+
+    def run():
+        pass
+
+    def handle_events():
+        pass
+    
+    def update():
+        pass
+    def check_collisions():
+        pass
+    
